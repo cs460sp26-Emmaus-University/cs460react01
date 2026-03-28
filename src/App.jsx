c0a6b8fd7bb86bda0a17f20c186f03a514db14d8
Professor Manning <atmanning@emmaus.edu>
2026-03-27 22:26:15 -0500

    Initialize React app with basic structure

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello, I am [Student Name]</h1>
+      <p>This is my first React app deployed on Render!</p>
+      <div className="card">
+        <p>Edit <code>src/App.jsx</code> to complete the assignment.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
